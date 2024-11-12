02368a4c802e52c16eba076f31937300a059f9a3
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2024-11-12 09:17:23 +0800

    Check whether kubectl connects to the right env before deploy

--- deploy-kmi-prod.sh
+++ deploy-kmi-prod.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+isprod=$(kubectl config get-contexts | grep  -E "\*\s+az-aks-prod01" | wc -l)
+if [[ ${isprod} -eq 1 ]]; then
+    echo "Kubectl is connected to az-aks-prod01, begin to deploy redis in prod env"
+else
+    echo "Kubectl is not connected to az-aks-prod01, can't deploy redis in prod env"
+    exit 1
+fi
+
 ./deploy.sh  upgrade --values values-geocluster-kmi-prod.yaml -n kmi kmi ./
