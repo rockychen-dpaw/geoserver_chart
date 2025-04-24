ddb6bcdbc3d5196053336358101df8cd6a5e3809
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2025-04-24 13:50:43 +0800

    files for kb prod

---
+++ deploy-kb-prod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+isuat=$(kubectl config get-contexts | grep  -E "\*\s+az-aks-prod01" | wc -l)
+if [[ ${isuat} -eq 1 ]]; then
+    echo "Kubectl is connected to az-aks-prod01, begin to deploy kb in prod env"
+else
+    echo "Kubectl is not connected to az-aks-prod01, can't deploy kb in prod env"
+    exit 1
+fi
+
+./deploy.sh  install --values values-geocluster-kb-prod.yaml -n kb kb ./
